38f56e1dad6a08a849f042e7e6ced5368fd2912e
dgjinsu <rlawlstn3206@naver.com>
2024-03-24 23:59:16 +0900

    Init: first commit

---
+++ src/main/java/poison/PoisonApplication.java
@@ -0,0 +1,13 @@
+package poison;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoisonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoisonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/poison/PoisonApplicationTests.java
@@ -0,0 +1,13 @@
+package poison;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoisonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
